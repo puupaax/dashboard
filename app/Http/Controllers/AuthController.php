b0221285c2aaf9914a39babf2e45436c6d81026a
Muhammad Ihsan <mihsan2903@gmail.com>
2024-09-16 14:14:21 +0700

    dashboard_manual

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth/register');
+    }
+
+    public function registerSave(Request $request)
+    {
+        Validator::make($request->all(), [
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required|confirmed'
+        ])->validate();
+
+        User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=> Hash::make($request->password),
+            'level' => 'Admin'
+        ]);
+        return redirect()->route('login');
+    }
+
+    public function login()
+    {
+        return view('auth/login');
+    }
+}
